a46976ea5a01fcaa49ac9877c278c910f25f9d38
Main Uddin <main06@gmail.com>
2018-09-18 12:19:40 -0400

    file has changed

---
+++ src/projectPackage/MyClass2.java
@@ -0,0 +1,15 @@
+package projectPackage;
+
+public class MyClass2 {
+// I have copied this class from Assignment1001Project2 
+	public static void main(String[] args) {
+		System.out.println("This class is very very  exciting"); // added very again
+		
+		System.out.println();
+		
+		//New line adding for Assignment1001Project3
+		System.out.println("What a beautiful job!");
+
+	}
+
+}
